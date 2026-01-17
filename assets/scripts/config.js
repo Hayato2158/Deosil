c34b2fd35b1154d81faa0851fe76b029ee2d4301
Hayato2158 <k.hayato2158@gmail.com>
2026-01-17 20:48:45 +0900

    fix：パスの修正

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,5 +3,5 @@ const isLocal = location.hostname === "localhost";
 window.DEOSIL_ENV = {
     SUPABASE_URL: "https://bllysyzdusuregqlraoi.supabase.co",
     SUPABASE_ANON_KEY: "sb_publishable_lVoteFia8h6EiXwdO7aqiA_Nn365Ewn",
-    BASE_PATH: "/Deosil",
+    BASE_PATH: isLocal ? "" : "/Deosil",
 };
