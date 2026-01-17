2baee6f27ef9162652830faa18c145f0a44e7803
Hayato2158 <k.hayato2158@gmail.com>
2026-01-17 20:46:03 +0900

    fix：検証先をlocalhostに変更

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,3 +1,5 @@
+const isLocal = location.hostname === "localhost";
+
 window.DEOSIL_ENV = {
     SUPABASE_URL: "https://bllysyzdusuregqlraoi.supabase.co",
     SUPABASE_ANON_KEY: "sb_publishable_lVoteFia8h6EiXwdO7aqiA_Nn365Ewn",
