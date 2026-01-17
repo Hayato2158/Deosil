6e29f188c235f836f7c7ebdaf18d6a89373f8d27
Hayato2158 <k.hayato2158@gmail.com>
2026-01-17 20:14:00 +0900

    add：検証用ディレクトリ作成

---
+++ notify-dev/config.js
@@ -0,0 +1,8 @@
+const REPO_BASE = "/notify-dev";
+const isNotifyDev = window.location.pathname.startsWith(REPO_BASE + "notify-dev);
+
+    window.DEOSIL_ENV = {
+        SUPABASE_URL: "https://bllysyzdusuregqlraoi.supabase.co",
+        SUPABASE_ANON_KEY: "sb_publishable_lVoteFia8h6EiXwdO7aqiA_Nn365Ewn",
+        BASE_PATH: BASE,
+    };
