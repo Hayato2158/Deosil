9d1ab7df836738400ce6b3a90c8f95fd2ff81433
Hayato2158 <k.hayato2158@gmail.com>
2026-01-17 20:30:19 +0900

    delete：notify-devの削除

--- notify-dev/config.js
+++
@@ -1,9 +0,0 @@
-const REPO_BASE = "/Deosil";
-const isNotifyDev = location.pathname.startsWith(REPO_BASE + "/notify-dev/");
-const BASE = isNotifyDev ? REPO_BASE + "/notify-dev" : REPO_BASE;
-
-window.DEOSIL_ENV = {
-    SUPABASE_URL: "https://bllysyzdusuregqlraoi.supabase.co",
-    SUPABASE_ANON_KEY: "sb_publishable_lVoteFia8h6EiXwdO7aqiA_Nn365Ewn",
-    BASE_PATH: BASE,
-};
